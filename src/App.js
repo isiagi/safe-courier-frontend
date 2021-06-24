39d4721cd275da35c9496d07989e40b18859b88f
isiagi <isiagigeofrey0@gmail.com>
2021-06-24 03:58:33 +0300

    adds edit component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Map from './componets/Dispaly/index'
 import Parcel from './componets/Parcel/Parcel'
 import ParcelID from './componets/Parcel/ParcelID'
 import ParcelCreate from './componets/Parcel/PostParcel'
+import EditParcel from './componets/Parcel/EditParcel'
 import Admin from './componets/Admin/Admin'
 
 
@@ -20,8 +21,9 @@ function App() {
             <Route path="/login" component={Login} exact/>
             <Route path="/map" component={Map} />
             <Route path="/parcels" component={Parcel} exact/>
-            <Route path="/parcels/:id" component={ParcelID}  exact/>
             <Route path="/parcels/new/parcel" component={ParcelCreate} exact/>
+            <Route path="/parcels/:id" component={ParcelID}  exact/>
+            <Route path="/parcels/:id/edit" component={EditParcel} exact/>
             <Route path="/admin" component={Admin} exact/>
         </Switch>
         </Router>
