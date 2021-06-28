04dd544646d42ca405ad906623996a71e079ee83
isiagi <isiagigeofrey0@gmail.com>
2021-06-28 04:08:51 +0300

    adds admin route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ParcelID from './componets/Parcel/ParcelID'
 import ParcelCreate from './componets/Parcel/PostParcel'
 import EditParcel from './componets/Parcel/EditParcel'
 import Admin from './componets/Admin/Admin'
+import AdminEdit from './componets/Admin/Edit'
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
             <Route path="/parcels/new/parcel" component={ParcelCreate} exact/>
             <Route path="/parcels/:id" component={ParcelID}  exact/>
             <Route path="/parcels/:id/edit" component={EditParcel} exact/>
+            <Route path="/parcels/:id/edit/admin" component={AdminEdit} exact/>
             <Route path="/admin" component={Admin} exact/>
         </Switch>
         </Router>
