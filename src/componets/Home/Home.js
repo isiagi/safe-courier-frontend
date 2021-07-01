ddfc4c13d9f19a1e6b858d0ef70422c464ba82dd
isiagi <isiagigeofrey0@gmail.com>
2021-07-01 10:07:45 +0300

    removes useEffect

--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { Link} from 'react-router-dom'
 import photo from '../images/1.jpg'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
