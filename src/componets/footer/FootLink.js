2a5c75a7ed3f9b5d17c86bb5f3e74cd429e5bfaa
isiagi <isiagigeofrey0@gmail.com>
2021-07-31 18:09:42 +0300

    adds more cases to switch

--- src/componets/footer/FootLink.js
+++ src/componets/footer/FootLink.js
@@ -15,34 +15,51 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column'
     },
   }));
+  
+  const pageTo = (page) => {
+    let day;
+    switch(page){
+        case 'Home':
+             day = "hello";
+             break;
+        case 'Contant Us':
+            day = 'hey there';
+            break;
+        case 'FAQ':
+            day = 'he';
+            break;
+        case 'Terms and Conditions':
+            day = 'h';
+            break;
+        case 'Help':
+            day = 'heyyz';
+            break;
+        case 'About Us':
+            day = 'heyyx';
+            break;
+        case 'Careers':
+            day = 'heyyw';
+            break;
+        case 'Capabilities':
+            day = 'heyyo';
+            break;
+        default:
+            day = 'hey'
+    }
+    return day
+}
 
 function FootLink({header, linkOne, linkTwo, linkThree, linkFour}) {
     const classes = useStyles();
-    console.log(linkOne)
 
-    const pageTo = (page) => {
-        let day;
-        switch(page){
-            case 'Home':
-                 day = "hello";
-                 break;
-            case 'Contant Us':
-                day = 'hey there';
-                break;
-            default:
-                day = 'hey'
-        }
-        console.log(day)
-        return day
-    }
     return (
         <div>
            <Box className={classes.supoortBox}>
           <h5>{header}</h5>
           <Link to={pageTo(linkOne)} className="supportLink">{linkOne}</Link>
-          <Link to="/#" className="supportLink">{linkTwo}</Link>
-          <Link to="/#" className="supportLink">{linkThree}</Link>
-          <Link to="/#" className="supportLink">{linkFour}</Link>
+          <Link to={pageTo(linkTwo)} className="supportLink">{linkTwo}</Link>
+          <Link to={pageTo(linkThree)} className="supportLink">{linkThree}</Link>
+          <Link to={pageTo(linkFour)} className="supportLink">{linkFour}</Link>
           </Box>
         </div>
     )
